d57250079608992ebad06b54210663f733703158
Itam67 <itamarpres@gmail.com>
2024-01-26 15:07:24 -0500

    Added dot product of unembedding with FV

--- notebooks/exploration.py
+++ notebooks/exploration.py
@@ -22,13 +22,35 @@ dataset = load_dataset('country-capital', seed=0)
 mean_activations = get_mean_head_activations(dataset, model, model_config, tokenizer)
 
 FV, top_heads = compute_universal_function_vector(mean_activations, model, model_config, n_top_heads=10)
+
+
+
+
+
+breakpoint()
 # %%
 dataset = load_dataset('country-capital')
 test_pairs = dataset['test'][:]
-print("here")
-#breakpoint()
+
+sims = []
+for i in range(len(test_pairs['input'])):
+    test_pair = [test_pairs['input'][i], test_pairs['output'][i]]
+    index = tokenizer(' '+test_pair[1])['input_ids'][0]
+    unembedding = model.lm_head.weight[index,:]
+    sims.append(FV[0].dot(unembedding).cpu().item())
+    
+plt.figure()
+plt.plot(sims)
+plt.title('Dot Products of FV with Answer Unembeddings')
+plt.xlabel('Prompt #')
+plt.ylabel('Dot Product')
+plt.savefig('FV_Dot.png')
+
+breakpoint()
 # total_diffs = []
 accs = []
+cos_sims = []
+
 for EDIT_LAYER in range(0,25):
     accuracy = 0
 
@@ -37,9 +59,9 @@ for EDIT_LAYER in range(0,25):
         test_pair = [test_pairs['input'][i], test_pairs['output'][i]]
         test_pair_dict = {'input': test_pair[0], 'output': test_pair[1]}
         zeroshot_prompt_data = word_pairs_to_prompt_data({'input':[], 'output':[]}, query_target_pair=test_pair_dict, prepend_bos_token=True, shuffle_labels=True)
+        
         zeroshot_sentence = create_prompt(zeroshot_prompt_data)
 
-        
         # Intervention on the zero-shot prompt
         clean_logits, interv_logits = function_vector_intervention(zeroshot_sentence, [test_pair[1]], EDIT_LAYER, FV, model, model_config, tokenizer)
        # breakpoint()
@@ -50,7 +72,6 @@ for EDIT_LAYER in range(0,25):
             #   tokenizer.decode(index),
             #   interv_logits[0].argmax().item()==index)
         if interv_logits[0].argmax().item()==index:
-            print("here")
             accuracy+=1
         
     # plt.plot(diffs)
